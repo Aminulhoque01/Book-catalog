3f225b15ddd6644c2b60aa656ba86804362cf054
Aminulhoque01 <aminuhoque53@gmail.com>
2023-07-16 10:35:04 +0600

    ten commit

--- src/pages/AddNewBook.tsx
+++ src/pages/AddNewBook.tsx
@@ -1,59 +1,85 @@
 import React from 'react';
 
 export default function AddNewBook() {
+
+  const handleAdd = (event:any) => {
+    event.preventDefault();
+    const form = event.target;
+    const items = form.items.value;
+    const price = form.price.value;
+    const textarea = form.textarea.value;
+    const genre = form.genre.value;
+    const url = form.url.value;
+    const used = form.used.value;
+    const PurchaseYear = form.PurchaseYear.value;
+    
+    console.log(items,price,textarea,genre,url,used,PurchaseYear)
+  }
   return (
-    <div className="block max-w-l  rounded-lg bg-black  p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <form>
-        <div className="relative mb-6" data-te-input-wrapper-init>
-          <input
-            type="text"
-            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-            id="exampleInput7"
-            placeholder="Name"
-          />
-          <label
-            for="exampleInput7"
-            className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-          >
-            Title
-          </label>
-        </div>
+    <div className='mx-auto w-96'>
+      <h2 className="text-3xl font-bold mb-5">Now add products</h2>
 
-        <div className="relative mb-6" data-te-input-wrapper-init>
-          <input
-            type="email"
-            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-            id="exampleInput8"
-            placeholder="Email address"
-          />
-          <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
-            image
-          </label>
-        </div>
-
+      <form onSubmit={handleAdd}>
+        <input
+          type="text"
+          name="items"
+          placeholder="Your book title"
+          className="input w-full input-bordered"
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="url"
+          placeholder="Your book image url"
+          className="input w-full input-bordered"
+        />
+        <br />
+        <br />
+        <textarea
+          
+          name="textarea"
+          placeholder="Your book description"
+          className="input w-full input-bordered"
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          name="price"
+          placeholder="Your book author name"
+          className="input w-full input-bordered"
+        />
+        <br />
+        <br />
        
 
-        <div className="relative mb-6" data-te-input-wrapper-init>
-          <textarea
-            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-            id="exampleFormControlTextarea13"
-            placeholder="Message"
-          ></textarea>
-          <label className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary">
-            Details
-          </label>
-        </div>
-
+        <input
+          type="text"
+          name="PurchaseYear"
+          placeholder="Your book reviews"
+          className="input w-full input-bordered"
+        />
+        <br />
+        <br />
+        <input
+          name="genre"
+          type="text"
+          placeholder="Your book Genre "
+          className="input w-full input-bordered"
+        />
+        <br />
+        <br />
+        <input
+          name="used"
+          type="text"
+          placeholder="publication year"
+          className="input w-full input-bordered"
+        />
+        <br />
+        <br />
        
-
-        <button
-          type="submit"
-          className="dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]] inline-block w-full rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-          data-te-ripple-init
-          data-te-ripple-color="light"
-        >
-          ADD book
-        </button>
+        <input  className="btn btn-accent w-full max-w-xs hover:cursor-pointer" type="submit" />
       </form>
     </div>
   );
